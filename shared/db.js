833a8d82ce25fb7b882006b96473908b196d190c
Mohammed AL-Hagreee <alhagree@yahoo.com>
2025-07-19 02:07:15 +0300

    update db.js

--- shared/db.js
+++ shared/db.js
@@ -1,4 +1,27 @@
 //backend\shared\db.js
+// ✅ فحص القيم البيئية المطلوبة
+const requiredEnvVars = [
+  "DB_HOST",
+  "DB_PORT",
+  "DB_USER",
+  "DB_PASS",
+  "DB_NAME"
+];
+
+const missingVars = requiredEnvVars.filter((key) => !process.env[key]);
+
+if (missingVars.length > 0) {
+  console.error("❌ القيم التالية مفقودة من environment:");
+  missingVars.forEach((key) => console.error(`- ${key}`));
+  process.exit(1); // إنهاء التطبيق لأن الاتصال سيفشل
+} else {
+  console.log("✅ جميع متغيرات البيئة موجودة بنجاح:");
+  requiredEnvVars.forEach((key) => {
+    console.log(`🔹 ${key} = ${process.env[key]}`);
+  });
+}
+
+
 const mysql = require("mysql2/promise");
 require("dotenv").config(); // لتحميل بيانات env
 
