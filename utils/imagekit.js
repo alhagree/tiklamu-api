e14b85f521688afa776e8de7444d36ca2fcf88dd
Mohammed AL-Hagreee <alhagree@yahoo.com>
2025-07-19 18:33:28 +0300

    integrate image upload to ImageKit for items, sections and settings

--- utils/imagekit.js
+++ utils/imagekit.js
@@ -3,7 +3,7 @@ const ImageKit = require("imagekit");
 
 const imagekit = new ImageKit({
   publicKey: "public_jDYvxg7atHgKBiE7QaFF2k/jZj4=",
-  privateKey: "private_TDYqLT1KjO******************",
+  privateKey: "private_TDYqLT1KjOUtOSpdKqtTBcO2tVw=",
   urlEndpoint: "https://ik.imagekit.io/idbeilkk4"
 });
 
