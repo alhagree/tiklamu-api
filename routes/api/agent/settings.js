adee074013b53452250799984d4182f401b84249
Mohammed AL-Hagreee <alhagree@yahoo.com>
2025-07-27 18:39:11 +0300

    fix: Fixde Path Save Image

--- routes/api/agent/settings.js
+++ routes/api/agent/settings.js
@@ -2,12 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const verifyToken = require("../../../middleware/verifyToken");
-//const controller = require("../../../controllers/agent/SettingsController");
-const {
-  getSettings,
-  updateSettings,
-  getFullSettingsWithSubscription,
-} = require("../../../controllers/agent/SettingsController");
+const controller = require("../../../controllers/agent/SettingsController");
 const multer = require("multer");
 
 // رفع صور متعددة (logo & background)
@@ -17,11 +12,11 @@ const upload = multer({ storage: multer.memoryStorage() }).fields([
 ]);
 
 // ✅ جلب الإعدادات
-router.get("/", verifyToken, getSettings);
+router.get("/", verifyToken, controller.getSettings);
 
-router.get("/subscription", verifyToken, getFullSettingsWithSubscription);
+router.get("/subscription", verifyToken, controller.getFullSettingsWithSubscription);
 
 // ✅ تعديل الإعدادات
-router.put("/", verifyToken, upload, updateSettings);
+router.put("/", verifyToken, upload, controller.updateSettings);
 
 module.exports = router;
