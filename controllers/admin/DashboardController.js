56fcd0d661e1982f8d40d8ea376a071a4216daac
Mohammed AL-Hagreee <alhagree@yahoo.com>
2025-08-02 16:31:16 +0300

    fix: Dashbord Status

--- controllers/admin/DashboardController.js
+++ controllers/admin/DashboardController.js
@@ -68,15 +68,20 @@ exports.getStats = async (req, res) => {
     if (clientId) {
       const [rows] = await db.query(
         `
-  SELECT DATE(DATE_ADD(vs_visit_time, INTERVAL 3 HOUR)) AS date, COUNT(*) AS count
+  SELECT 
+    DATE(DATE_ADD(vs_visit_time, INTERVAL 3 HOUR)) AS visit_date, 
+    COUNT(*) AS count
   FROM visits
-  WHERE vs_us_link_code = (
-    SELECT us_link_code FROM us_users WHERE us_client_id = ? LIMIT 1
+  WHERE vs_us_link_code COLLATE utf8mb4_general_ci = (
+    SELECT us_link_code COLLATE utf8mb4_general_ci 
+    FROM us_users 
+    WHERE us_client_id = ? 
+    LIMIT 1
   )
   AND vs_visit_time >= DATE_SUB(CURDATE(), INTERVAL 6 DAY)
-  GROUP BY DATE(DATE_ADD(vs_visit_time, INTERVAL 3 HOUR))
-  ORDER BY DATE(DATE_ADD(vs_visit_time, INTERVAL 3 HOUR))
-`,
+  GROUP BY visit_date
+  ORDER BY visit_date
+  `,
         [clientId]
       );
 
