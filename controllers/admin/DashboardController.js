08d7ca6c2031faa2925d5c3a4aaa7ec8de5fb98e
Mohammed AL-Hagreee <alhagree@yahoo.com>
2025-08-02 15:43:40 +0300

    fix: Dashbord Status

--- controllers/admin/DashboardController.js
+++ controllers/admin/DashboardController.js
@@ -48,7 +48,7 @@ exports.getStats = async (req, res) => {
       "SELECT COUNT(*) AS totalRequests FROM subscription_requests"
     );
     const [[{ newRequests }]] = await db.query(
-      "SELECT COUNT(*) AS newRequests FROM subscription_requests WHERE sl_status = 1"
+      "SELECT COUNT(*) AS newRequests FROM subscription_requests WHERE sr_status = 1"
     );
 
     // تخزين القيم
